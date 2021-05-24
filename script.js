213565ab898c001ed1b7704c5ac9b6980263cceb
aaronh1986 <aaronharbourne@gmail.com>
2021-05-24 12:05:48 +0000

    Finished

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container');
 const questionElement = document.getElementById('question');
 const answerButtonsElement = document.getElementById('answer-buttons');
@@ -6,6 +7,10 @@ const answerButtonsElement = document.getElementById('answer-buttons');
 let shuffledQuestions, currentQuestionIndex;
 
 startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion();
+})
 
 function startGame() {
   console.log('started');
@@ -17,6 +22,7 @@ function startGame() {
 }
 
 function setNextQuestion() {
+    resetState();
   showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
@@ -29,12 +35,47 @@ function showQuestion(question) {
       if(answer.correct) {
           button.dataset.correct = answer.correct
       }
-      button.addEventListener('click', selectAnswer)
+      button.addEventListener('click', selectAnswer);
+      answerButtonsElement.appendChild(button);
   })
 }
 
-function selectAnswer() {
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide');
+    while(answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+    }
+}
 
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach(button => {
+      setStatusClass(button, button.dataset.correct);
+  });
+  if(shuffledQuestions.length > currentQuestionIndex + 1) {
+      nextButton.classList.remove('hide');
+  } else {
+      startButton.innerText = "restart";
+      startButton.classList.remove('hide');
+  }
+  nextButton.classList.remove('hide');
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
 const questions = [
@@ -44,5 +85,19 @@ const questions = [
             {text: '4', correct: true},
             {text: '22', correct: false}
         ]
-    }
+    },
+    {
+        question: 'Who is the greatest boxer?',
+        answers: [
+            {text: 'Tyson Fury', correct: true},
+            {text: 'Deontay Wilder', correct: false}
+        ]
+    },
+    {
+        question: 'What is 2 * 16?',
+        answers: [
+            {text: '32', correct: true},
+            {text: '1000', correct: false}
+        ]
+    },
 ]
